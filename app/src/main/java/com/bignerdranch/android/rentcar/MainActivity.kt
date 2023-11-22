b6557e17f1f8779ac171e1540bbc228f6e961c67
ikstami <k.mus00@yandex.ru>
2023-11-22 12:36:28 +0700

    Вторая активность

--- app/src/main/java/com/bignerdranch/android/rentcar/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/rentcar/MainActivity.kt
@@ -8,26 +8,38 @@ import android.widget.EditText
 import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
-    private  lateinit var numberHours: EditText
-    private  lateinit var auto: RadioButton
-    private  lateinit var van: RadioButton
-    private  lateinit var bus: RadioButton
-    private  lateinit var okButton: Button
+    private lateinit var numberHours: EditText
+    private lateinit var auto: RadioButton
+    private lateinit var van: RadioButton
+    private lateinit var bus: RadioButton
+    private lateinit var okButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        numberHours = findViewById(R.id.numberHours)
+        auto = findViewById(R.id.auto)
+        van = findViewById(R.id.van)
+        bus = findViewById(R.id.bus)
+        okButton = findViewById(R.id.okButton)
 
         okButton.setOnClickListener{
             val hours = numberHours.text.toString().toIntOrNull()
-            val car = when {
-                auto.isChecked -> 2000
-                van.isChecked -> 2500
-                bus.isChecked -> 3000
-                else -> 0
+        val car = when {
+            auto.isChecked -> 0.03
+            van.isChecked -> 0.05
+            bus.isChecked -> 0.09
+            else -> 0.0
+        }
+            if(hours != null) {
+                val result = hours * car
+                val intent = Intent(this, ResultActivity::class.java).apply {
+                    putExtra("result", result)
+                }
+                startActivity(intent)
             }
-
-
         }
 
     }
 }
+
+
